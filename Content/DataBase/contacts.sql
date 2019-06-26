e5a30d6f54c8f75cf65edd3c2f8d030964f6918b
NicoBecq <becquerel.nicolas@outlook.fr>
2019-06-26 15:42:02 +0200

    add db.sql

---
+++ Content/DataBase/contacts.sql
@@ -0,0 +1,33 @@
+--
+-- Base de données :  `crudbrut`
+--
+CREATE DATABASE IF NOT EXISTS `crudbrut` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `crudbrut`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `idContact` int(11) NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  PRIMARY KEY (`idContact`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `contacts`
+--
+
+--INSERT INTO `contacts` (`idContact`, `lastName`, `firstName`, `mail`, `phoneNumber`) VALUES
+--(1, 'Becquerel', 'Nicolas', 'nicobecqu@gmail.com', '0102030405'),
+--(13, 'text', 'test', 'text@test.com', '0504030201'),
+--(14, 'text', 'test', 'text@test.com', '0504030201'),
+--(15, 'text', 'test', 'text@test.com', '0504030201'),
+--(21, 'boloss', 'bobo', 'boboboloss@boloss.bo', '0506070809'),
+--(20, 'becquerel', 'nicolas', 'becquerel.nicolas@outlook.fr', '0102030405');
